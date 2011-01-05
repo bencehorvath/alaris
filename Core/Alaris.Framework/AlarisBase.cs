6602338dc4030c62f1c37656e0084c3bb27a0648
Twl <twl@null.net>
2011-01-05 19:43:28 +0100

    * Add the empty abstract base class AlarisBase.

---
+++ Core/Alaris.Framework/AlarisBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alaris.Framework
+{
+    ///<summary>
+    /// Base abstract class for Alaris-based bots.
+    ///</summary>
+    ///<typeparam name="T"></typeparam>
+    public abstract class AlarisBase<T>
+        where T: class, new()
+    {
+
+    }
+}
